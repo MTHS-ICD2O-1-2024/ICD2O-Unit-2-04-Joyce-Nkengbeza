d2a057a3779d20e441f624df0484e6189652fd5e
default <Joyce.nkengbeza@stu.ocsb.ca>
2025-03-20 16:52:20 +0000

    template

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Joyce Nkengbeza
+// Created on: Feb 2025
 // This file contains the JS functions for index.html
 
-/**
- * Displays an alert with the message "Hello, World!"
- */
-function myButtonClicked() {
-  alert("Hello, World!")
+function enterClicked() {
+  // input
+  const streetnumber = parseInt(document.getElementById("street-number").value)
+  const streetname = document.getElementById("street-name").value
+  // output
+  document.getElementById("street-adress").innerHTML =
+    "Your Adress Is: " + streetnumber + " " + streetname + "."
 }
